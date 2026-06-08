b87067529132ac763a13366f12b5360481aeaf1b
Mallory Rich <mrich369@byu.edu>
2026-06-08 11:44:23 -0600

    car stuff

---
+++ day_11/car.py
@@ -0,0 +1,94 @@
+"""
+PIPELINE
+- EPA API (real MPG and fuel cost)
+- CarEdge Scrape
+- Pandas (combine and clean)
+- TCO Model (formula we design)
+- Recommend (chart)
+"""
+
+import requests, time
+from bs4 import BeautifulSoup
+
+def get_fuel_data (year, make, model):
+
+    """
+    Inputs:
+    - year int
+    - make str
+    - model str
+    Processes:
+     - look up car ID using fueleconomy.gov
+     - use car id to get the comb08 mpg
+    Outputs:
+    - combined mpg
+    Notes:
+        https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2020&make=Honda&model=Civic%204Dr
+        https://www.fueleconomy.gov/ws/rest/vehicle/42149
+    """
+    
+    if year < 1900 and year > 2100:
+        print("Please enter a reasonable year.")
+        
+    base_url = "https://www.fueleconomy.gov/ws/rest/"
+    base_headers = headers={"Accept":"application/json"}
+
+    url = base_url + f"vehicle/menu/options?year={year}&make={make}&model={model}"
+    response = requests.get(url, headers=base_headers)
+    data = response.json()
+    vehicle_id = data["menuItem"][0]["value"]
+
+    # second call to grab MPG for vehicle ID
+    url = base_url + f"vehicle/{vehicle_id}"
+    response = requests.get(url, headers=base_headers)
+    data = response.json()
+
+    return data["comb08"]
+
+# SCRAPING THE WEB
+
+def get_maitenance_cost(make, model):
+    """
+    I: make (str), model (str)
+
+    P: scrape data to get 10-year maitenance costs for make and model
+
+    O: 10-year cost (int)
+
+    Notes: 
+        use CarEdge.com
+        https://caredge.com/toyota/camry/maintenance
+    """
+    base_url = "https://caredge.com/"
+    base_headers = {"User-Agent": "Mozilla/5.0"}
+
+    url = base_url + f"{make.lower()}/{model.lower()}/maintenance"
+    response = requests.get(url, headers=base_headers)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find_all("table")[0]
+    total_maitenance = 0
+    for row in table.find_all("tr")[1:]:
+        cells = [td.get_text(strip=True) for td in row.find_all("td")]
+        dollar_amount = cells[2]
+        dollar_amount = dollar_amount.replace("$","")
+        dollar_amount = dollar_amount.replace(",","")
+        int_amount = int(dollar_amount)
+        total_maitenance += int_amount
+        
+    return total_maitenance
+
+list_of_vehicles = [
+    {"year": 2020, "make": "Honda", "model": "Civic", "extra_text": " 4Dr"},
+    {"year": 2020, "make": "Chevrolet", "model": "Blazer", "extra_text": " AWD"},
+    {"year": 2021, "make": "Subaru", "model": "Outback", "extra_text": " AWD"}
+]
+
+for vehicle in list_of_vehicles:
+    mpg = get_fuel_data(vehicle["year"], vehicle["make"], vehicle["model"]+vehicle["extra_text"])
+    ten_year_maitenance = get_maitenance_cost(vehicle["make"], vehicle["model"])
+    tco = (11000*10)/int(mpg)*4.50 + ten_year_maitenance
+    if (int(mpg)) < 5:
+        print("No MPG data.")
+    print(f"{vehicle["year"]} {vehicle["make"]} {vehicle["model"]}{vehicle["extra_text"]}\nTCO: ${tco:.2f}")
+    time.sleep(5)
